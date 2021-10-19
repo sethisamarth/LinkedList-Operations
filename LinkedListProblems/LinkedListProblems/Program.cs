fa70ad91168b4e4699853f0ac502d369dd0b51c3
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-19 20:58:24 +0530

    Inserting at front

--- LinkedListProblems/LinkedListProblems/Program.cs
+++ LinkedListProblems/LinkedListProblems/Program.cs
@@ -6,7 +6,15 @@ namespace LinkedListProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LinkedList!");
+            //UseCase 1
+            Operations obj = new Operations();
+            //Insertion Operation
+            obj.InsertFront(56);
+            obj.InsertFront(30);
+            obj.InsertFront(70);
+            //Display Operation
+            Console.WriteLine("*****Insertion at First*****");
+            obj.Display();
         }
     }
 }

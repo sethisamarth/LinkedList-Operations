38d60e88ff24ace788d4b8d7bf3145b4e68550e2
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-19 20:37:39 +0530

    Welcome to Linkedlist Operations

---
+++ LinkedListProblems/LinkedListProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList!");
+        }
+    }
+}
